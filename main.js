a2c7a3a20f305473efc729790988b7bb3db78248
nawarzaki <42673579+nawarzaki@users.noreply.github.com>
2018-12-28 23:46:39 +0300

    jhjhjhjhj

--- main.js
+++ main.js
@@ -1,34 +1,27 @@
-
-
-function createCommentBody(comment) {
-  var bodyDiv = document.createElement("div");
-  var bodyPara = document.createElement("li");
-  bodyPara.innerHTML = comment;
-  bodyDiv.appendChild(bodyPara);
-  return bodyDiv;
-}
-
-
 function postNewComment(body) {
   var commentsDiv = document.getElementById("comments");
-  var newCommentDiv = document.createElement("div");
-  newCommentDiv.appendChild(body);
-   commentsDiv.appendChild(newCommentDiv)
+  var newCommentDiv = document.createElement("li");
+  newCommentDiv.innerHTML = body;
+  commentsDiv.appendChild(newCommentDiv)
 }
 
 function addComment() {
   event.preventDefault();
   var bodyText = document.getElementById("commentText").value;
-  if( bodyText.trim() === "") {
+  if (bodyText.trim() === "") {
     alert("Please write somthing");
-} else {
-  bodyText 
-}
-    var commentBody = createCommentBody(bodyText);
-    postNewComment(commentBody);
+  } else {
+    bodyText
+    commentText.value = ""
+  }
+  var commentBody = postNewComment(bodyText);
+
 }
 
 function removeDummy() {
-var elem = document.getElementById("comments");
-elem.parentNode.removeChild(elem)
+  var elem = document.getElementById("comments");
+  elem.parentNode.removeChild(elem)
 }
+
+
+
